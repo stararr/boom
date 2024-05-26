9f9bbc85e24a735f023506784bdee30e68c068c1
stararr <132271494+stararr@users.noreply.github.com>
2024-05-26 22:17:32 +0530

    Update functions.js

--- functions.js
+++ functions.js
@@ -5,17 +5,21 @@ var loadingmessages =
 " uses Python to automate literally everything, though the files aren't uploaded to Neocities.",
 " will stop updating this on June 3rd, 2024.",
 " drew the star for the loading screen MULTIPLE times.",
-" thinks that there is no perfect star... And no perfect Star, either.",
+" thinks that there is no perfect star... And no perfect <b>Star</b>, either.",
 " loves her dragons in <a href=\"https://www.roblox.com/games/3475397644/Dragon-Adventures-Fantasy-Pets\">Dragon Adventures</a>.",
-"'s favorite vocaloid is vflower!"]
-var stars = ["./images/star.png", "./images/star_happy.png", "./images/star_contented.png", "./images/star_sleepy.png"]
-var loaded, currentsoundtobeloaded;
+"'s favorite vocaloid is vflower!",
+" loves strawberry ice cream.",
+" took one hour to finally be happy with the shape of the star in the loading screen.",
+" needs suggestions for more modules... 3:"]
+var stars = ["./images/star.png", "./images/star_happy.png", "./images/star_contented.png", "./images/star_sleepy.png", "./images/star_colonthree.png", "./images/star_derp.png", "./images/star_sleepy.png"]
+var loadedsounds = 0;
+var currentsoundtobeloaded;
 var squeaks = Array();
 for (sound of ["https://files.catbox.moe/59qqdi.mp3", "https://files.catbox.moe/tsg5iw.mp3", "https://files.catbox.moe/pkeyqk.mp3"]) {
     sound=new Audio(sound);
     squeaks.push(sound);
     sound.addEventListener('canplaythrough', () => {
-        loaded++
+        loadedsounds++
         }, false);
 }
 var pops = Array();
@@ -23,29 +27,32 @@ for (sound of ["https://files.catbox.moe/9tsqjp.mp3", "https://files.catbox.moe/
     sound=new Audio(sound);
     pops.push(sound);
     sound.addEventListener('canplaythrough', () => {
-        loaded++
+        loadedsounds++
         }, false);
 }
+
 
 var showLoadingMessages = window.setInterval(function(){
     document.getElementById('didyouknow').innerHTML="<b>did you know? star</b>"+loadingmessages[Math.round(Math.random()*(loadingmessages.length-1))];
     document.getElementById('star').src = stars[Math.round(Math.random()*(stars.length-1))];
-}, 1000);
+}, 1250);
+
 
 function onReady(callback) {
     var intervalId = window.setInterval(function() {
-        if ((document.body !== undefined) && (loaded=7)) {
+        if ((document.body !== undefined) && (loadedsounds==7)) {
             window.clearInterval(showLoadingMessages);
+            window.clearInterval(intervalId);
             callback.call(this);
         }
-    }, 5000);
+    }, 7500);
 }
   
 function setVisible(id, visible) {
-    document.getElementById(id).style.animation = 'loaded 0.5s ease-out'
+    document.getElementById('container').style.display='flex';
+    document.getElementById(id).style.animation = 'loaded 2500ms ease-out';
     document.getElementById(id).addEventListener('animationend', () => {
         document.getElementById(id).style.display = visible ? 'flex' : 'none';
-        clearTimeout(showLoadingMessages);
     })
 }
 
@@ -164,7 +171,10 @@ function bnyCreate() {
     for (i=0; i<8; i++) {
         randombny+=String(Math.round(Math.random()))
         document.getElementsByClassName('boy')[i].style.backgroundColor = (randombny[i]=="0") ? 'cyan' : 'yellow';
+        document.getElementsByClassName('boy')[i].style.boxShadow = (randombny[i]=="0") ? '0 0 0.5rem 0.125rem cyan' : '0 0 0.5rem 0.125rem yellow';
     }
+    document.getElementById('3').getElementsByClassName('currentstatus')[0].innerText='Current position: 1/8';
+
 }
 bnyCreate();
 
@@ -176,7 +186,7 @@ function resetReaction() {
     for (incr of [0,1]) {
         square = document.getElementsByClassName('borsquare')[incr];
         firstpart = (reaction[incr]>0) ? '<div class=\"dash\"></div>'.repeat(reaction[incr]) : "";
-        secondpart = ((3-reaction[incr])>0) ? '<div></div>'.repeat(3-reaction[incr]) : "";
+        secondpart = ((3-reaction[incr])>0) ? '<div class=\"foodash\"></div>'.repeat(3-reaction[incr]) : "";
         square.innerHTML = firstpart+secondpart;
     }
     reactionanswer = (reaction[0]>reaction[1]) ? 'R' : 'B' ;
@@ -201,10 +211,11 @@ function updateReaction(button) {
 var sets = [["A","B"],["C","D"],["E","F"],["G","H"],["I","J"],["K","L"],["M","N"],["O","P"],["Q","R"],["S","T"],["U","V"],["W","X"],["Y","Z"]];
 var currentset, key, filler, datastring, letterstring, inputdata, currentdataindex, currentdata;
 function createDataString() {
-    for (circle of document.getElementsByClassName('datacircle')) {circle.style.backgroundColor='slategrey';}
+    for (circle of document.getElementsByClassName('datacircle')) {circle.style.backgroundColor='slategrey'; circle.style.border='rgba(31, 42, 66, 0.5) 0.25rem outset'}
     currentdataindex = 0;
     currentdata=document.getElementsByClassName('datacircle')[currentdataindex];
-    currentdata.style.borderColor='skyblue';
+    currentdata.style.border='pink 0.25rem inset';
+    currentdata.style.boxShadow='0 0 0.25rem 0.125rem pink';
     gamemodule=document.getElementById('5');
     currentset = sets[Math.round(Math.random()*(sets.length-1))];
     key = currentset[Math.round(Math.random())];
@@ -229,7 +240,8 @@ function createDataString() {
     document.getElementById('datadisplay').innerText=letterstring;
 }
 function nextData(button) {
-    currentdata.style.borderColor = 'black';
+    currentdata.style.border='rgba(31, 42, 66, 0.5) 0.25rem outset';
+    currentdata.style.boxShadow='none';
     inputdata += (currentdata.style.backgroundColor=='slategrey') ? '0' : '1';
     // after the previous data circle
     if (currentdataindex==4) {
@@ -245,7 +257,8 @@ function nextData(button) {
     else {
         currentdataindex+=1;
         currentdata=document.getElementsByClassName('datacircle')[currentdataindex];
-        currentdata.style.borderColor='skyblue';
+        currentdata.style.border='pink 0.25rem inset';
+        currentdata.style.boxShadow='0 0 0.25rem 0.125rem pink';
     }
 }
 function toggleData() {
@@ -259,7 +272,7 @@ function resetSwitches() {
         switchy.style.backgroundImage="linear-gradient(grey 0%, whitesmoke 50%, whitesmoke 100%)";
         switchy.toggle = 'off';
     }
-    for (switchylighty of document.getElementsByClassName('switchlight')) {switchylighty.style.backgroundColor = 'red';}
+    for (switchylighty of document.getElementsByClassName('switchlight')) {switchylighty.style.backgroundColor = 'crimson';}
     inputswitch=[0,0,0,0];
     switchstring="";
     inputstring="";
@@ -278,13 +291,13 @@ function switchSwitch(switchy) {
             switchy.style.backgroundImage="linear-gradient(whitesmoke 0%, whitesmoke 50%, grey 100%)";
             inputswitch[switchy.switchnumber]=1;
             switchy.toggle='on';
-            document.getElementsByClassName('switchlight')[switchy.switchnumber].style.backgroundColor = 'limegreen';
+            document.getElementsByClassName('switchlight')[switchy.switchnumber].style.backgroundColor = 'springgreen';
             break;
         case 'on':
             switchy.style.backgroundImage="linear-gradient(grey 0%, whitesmoke 50%, whitesmoke 100%)"
             inputswitch[switchy.switchnumber]=0;
             switchy.toggle='off';
-            document.getElementsByClassName('switchlight')[switchy.switchnumber].style.backgroundColor = 'red';
+            document.getElementsByClassName('switchlight')[switchy.switchnumber].style.backgroundColor = 'crimson';
             break;
         
     }
@@ -467,15 +480,16 @@ function changeOpacityOfUI(slider) {
 function bnyAdd(button, value) {
     inputbny+=String(value);
     pop();
-    if (inputbny.length==8) {
-        if (inputbny==randombny) {
+    if (inputbny==randombny.slice(0,inputbny.length)) {
+        document.getElementById('3').getElementsByClassName('currentstatus')[0].innerText='Current position: '+String(inputbny.length+1)+'/8';
+        if (inputbny.length==8) {
             updatepoints(3, 1);
             bnyCreate();
         }
-        else {
-            updatepoints(3, -1);
-            inputbny="";
-        }
+    }
+    else {
+        updatepoints(3, -1);
+        inputbny="";
     }
 }
 
@@ -501,6 +515,7 @@ function toggleLightOrDark(button) {
             lightordark = '0,0,0,';
             button.value = "dark mode";
             button.style.backgroundColor = 'black';
+            button.style.borderColor='black';
             button.style.color='white';
             break;
         case "dark mode":
@@ -508,6 +523,7 @@ function toggleLightOrDark(button) {
             lightordark = '255,255,255,';
             button.value = "light mode";
             button.style.backgroundColor = 'white';
+            button.style.borderColor='white';
             button.style.color = 'black';
             break;
     }
@@ -553,14 +569,19 @@ function json(url) {
 
 function doxxYourself() {
     jumpscare('./images/good_morning_june.png', 500);
-    json(`https://api.ipdata.co?api-key=${'1c571f4896ad7cf2707e1fb9bf45b6965841199af3e9eab0f47aea5c'}`).then(data => {
-        window.alert('Your ip is: '+data.ip);
-        window.alert('Your country is '+(((data.is_eu)?'':'not '))+'a part of the European Union.');
-        window.alert('You live in '+((data.country_name[0]=='U')?'the ':'')+data.country_name+'.');
-        window.alert('You live in the city of '+data.city+'.');
-        window.alert('You live in the '+(data.region_type.toLowerCase())+' of '+data.region+'.');
-        window.alert('Your country\'s calling code is '+String(data.calling_code)+'.');
-        if (data.postal_code) {window.alert('Your country\'s postal code is '+String(data.postal_code)+'.');}
-        window.alert('You are currently at '+String(data.latitude)+' latitude and '+String(data.longitude)+' longitude.');
-    })
+    try {
+        json(`https://api.ipdata.co?api-key=${'1c571f4896ad7cf2707e1fb9bf45b6965841199af3e9eab0f47aea5c'}`).then(data => {
+            window.alert('Your ip is: '+data.ip);
+            window.alert('Your country is '+(((data.is_eu)?'':'not '))+'a part of the European Union.');
+            window.alert('You live in '+((data.country_name[0]=='U')?'the ':'')+data.country_name+'.');
+            window.alert('You live in the city of '+data.city+'.');
+            window.alert('You live in the '+(data.region_type.toLowerCase())+' of '+data.region+'.');
+            window.alert('Your country\'s calling code is '+String(data.calling_code)+'.');
+            if (data.postal_code) {window.alert('Your country\'s postal code is '+String(data.postal_code)+'.');}
+            window.alert('You are currently at '+String(data.latitude)+' latitude and '+String(data.longitude)+' longitude.');
+        })
+    }
+    catch {
+        window.alert('sorry, there was an error trying to fetch your ip.\ni hope you liked the jumpscare though! ^^');
+    }
 }
